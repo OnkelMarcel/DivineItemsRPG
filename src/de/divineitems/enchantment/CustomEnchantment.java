bcac6bd81c50ee83457595eb3e3ec1942fe5cc7b
OnkelMarcel <mrcelkrem30@yahoo.com>
2018-11-26 22:44:15 +0100

    Update CustomEnchantment.java

--- src/de/divineitems/enchantment/CustomEnchantment.java
+++ src/de/divineitems/enchantment/CustomEnchantment.java
@@ -1,94 +1,90 @@
 package de.divineitems.enchantment;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public abstract class CustomEnchantment {
+public class CustomEnchantment {
 	
-	private String name, level;
-	private int successRate, failureRate, chance;
+	private String name;
+	
 	private List<String> lore;
-	public static HashMap<String, CustomEnchantment> names = new HashMap<>();
+	private List<String> realLore;
 	
-	public CustomEnchantment(String name, String level, int successRate, int failureRate, int chance) {
+	private EnchantConfig ec;
+
+	public CustomEnchantment(String name) {
 		this.name = name;
-		this.level = level;
-		this.successRate = successRate;
-		this.lore = new ArrayList<>();
+		this.ec = new EnchantConfig(name);
 	}
 	
-	public CustomEnchantment() {
-		this.lore = new ArrayList<>();
+	public void loadBook() {
+		this.name = ec.getString("Displayname");
+		this.lore = ec.getStringList("Lore");
+		this.realLore = new ArrayList<>();
 	}
 	
-	
-	public ItemStack getItem() {
-		ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
-		ItemMeta im = book.getItemMeta();
-		im.setDisplayName("§c(Enchant) " + this.name + " " + this.level);
-		im.setLore(this.lore);
-		book.setItemMeta(im);
-		return book;
+	private ItemStack replaceItem(ItemStack old, String addToLore) {
+		ItemStack newItem = old;
+		ItemMeta im = newItem.getItemMeta();
+		List<String> l = new ArrayList<>();
+		for(String line : im.getLore()) {
+			l.add(line);
+		}
+		l.add(addToLore);
+		im.setLore(l);
+		newItem.setItemMeta(im);
+		return newItem;
 	}
 	
-	public void setLore(String description) {
-		this.lore.add("---------- Info ----------");
-		this.lore.add("> Type: Divine Enchant");
-		this.lore.add("> Level: " + this.level);
-		this.lore.add("> Success: " + this.successRate + " : Failure: " + this.failureRate);
-		this.lore.add("---------- Description ----------");
-		this.lore.add("You have a chance " + this.chance + " to " + description);
-		this.lore.add("");
-		this.lore.add("(Drag on item to enchant)");
+	public void addCustomEnchant(String enchant, Player p, ItemStack toEnchant) {
+		p.getInventory().addItem(replaceItem(toEnchant, enchant));
+		p.getInventory().remove(toEnchant);
+		p.updateInventory();
 	}
 	
-	public void setChance(int chance) {
-		this.chance = chance;
-	}
-	
-	public int getChance() {
-		return this.chance;
-	}
-	
-	public int getFailureRate() {
-		return this.failureRate;
-	}
-	
-	public void setFailureRate(int failureRate) {
-		this.failureRate = failureRate;
-	}
-	
-	public void setSuccessRate(int successRate) {
-		this.successRate = successRate;
-	}
-	
-	public int getSuccessRate() {
-		return this.successRate;
-	}
-	
-	public List<String> getLore() {
-		return this.lore;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
+	public ItemStack getBook() {
+		ItemStack is = new ItemStack(Material.ENCHANTED_BOOK);
+		ItemMeta im = is.getItemMeta();
+		im.setDisplayName(name);
+		
+		for(String line : lore) {
+			line = ChatColor.GRAY + line;
+			realLore.add(line);
+		}
+		
+		im.setLore(realLore);
+		is.setItemMeta(im);
+		return is;
 	}
 	
 	public String getName() {
-		return this.name;
+		return name;
 	}
-	
-	public void setLevel(String level) {
-		this.level = level;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	
-	public String getLevel() {
-		return this.level;
+
+	public List<String> getLore() {
+		return lore;
+	}
+
+	public void setLore(List<String> lore) {
+		this.lore = lore;
+	}
+
+	public EnchantConfig getIc() {
+		return ec;
+	}
+
+	public void setIc(EnchantConfig ec) {
+		this.ec = ec;
 	}
 	
 }
