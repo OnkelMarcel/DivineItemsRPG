6e34efd4bf4689685cf35883f4cb2882cb727edc
OnkelMarcel <mrcelkrem30@yahoo.com>
2018-11-26 22:46:27 +0100

    Update DivineItemsMain.java

--- src/de/divineitems/main/DivineItemsMain.java
+++ src/de/divineitems/main/DivineItemsMain.java
@@ -1,12 +1,17 @@
 package de.divineitems.main;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.divineitems.commands.GetEnchantedBookCMD;
 import de.divineitems.commands.GetItemCMD;
 import de.divineitems.commands.ShowLoadedItemsCMD;
-import de.divineitems.enchantment.ExecutionerEnchant;
+import de.divineitems.enchantment.CustomEnchantment;
+import de.divineitems.enchantment.EnchantManager;
 import de.divineitems.events.AddEnchToItemEvent;
 import de.divineitems.events.EntityDamageEvent;
 import de.divineitems.item.CustomItem;
@@ -15,6 +20,7 @@ import de.divineitems.item.ItemManager;
 public class DivineItemsMain extends JavaPlugin {
 	
 	public static ItemManager IM = new ItemManager();
+	public static EnchantManager EM = new EnchantManager();
 	
 	@Override
 	public void onEnable() {
@@ -22,10 +28,28 @@ public class DivineItemsMain extends JavaPlugin {
 		loadConfig();
 		registerEvents(getServer().getPluginManager());
 		
+		try {
+			getConfig().load(new File("plugins/Items/Items.yml"));
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+		}
+		
 		for(String s : getConfig().getKeys(false)) {
 			CustomItem ci = new CustomItem(s);
 			ci.loadItem();
 			IM.addItem(ci.getName(), ci);
+		}
+		
+		try {
+			getConfig().load(new File("plugins/enchantments/enchant.yml"));
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+		for(String s : getConfig().getKeys(false)) {
+			CustomEnchantment ce = new CustomEnchantment(s);
+			ce.loadBook();
+			EM.addEnchantment(ce.getName(), ce);
 		}
 		
 		System.out.println("[DivineItems] Enabled");
@@ -43,7 +67,6 @@ public class DivineItemsMain extends JavaPlugin {
 	private void registerEvents(PluginManager pm) {
 		pm.registerEvents(new EntityDamageEvent(), this);
 		pm.registerEvents(new AddEnchToItemEvent(), this);
-		pm.registerEvents(new ExecutionerEnchant(), this);
 	}
 	
 	private void loadCommands() {
