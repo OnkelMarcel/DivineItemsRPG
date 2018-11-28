170081228acb961114effdfe0ab337728cb8fd51
OnkelMarcel <mrcelkrem30@yahoo.com>
2018-11-28 01:33:41 +0100

    Update CustomEnchantment.java

--- src/de/divineitems/enchantment/CustomEnchantment.java
+++ src/de/divineitems/enchantment/CustomEnchantment.java
@@ -9,31 +9,33 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import de.divineitems.main.DivineItemsMain;
+
 public class CustomEnchantment {
-	
+
 	private String name;
-	
+
 	private List<String> lore;
 	private List<String> realLore;
-	
+
 	private EnchantConfig ec;
 
 	public CustomEnchantment(String name) {
 		this.name = name;
 		this.ec = new EnchantConfig(name);
 	}
-	
+
 	public void loadBook() {
 		this.name = ec.getString("Displayname");
 		this.lore = ec.getStringList("Lore");
 		this.realLore = new ArrayList<>();
 	}
-	
+
 	private ItemStack replaceItem(ItemStack old, String addToLore) {
 		ItemStack newItem = old;
 		ItemMeta im = newItem.getItemMeta();
 		List<String> l = new ArrayList<>();
-		for(String line : im.getLore()) {
+		for (String line : im.getLore()) {
 			l.add(line);
 		}
 		l.add(addToLore);
@@ -41,28 +43,29 @@ public class CustomEnchantment {
 		newItem.setItemMeta(im);
 		return newItem;
 	}
-	
+
 	public void addCustomEnchant(String enchant, Player p, ItemStack toEnchant) {
-		p.getInventory().addItem(replaceItem(toEnchant, enchant));
-		p.getInventory().remove(toEnchant);
-		p.updateInventory();
+		if (DivineItemsMain.EM.getEnchantment(enchant) != null) {
+			p.getInventory().addItem(replaceItem(toEnchant, enchant));
+			p.updateInventory();
+		}
 	}
-	
+
 	public ItemStack getBook() {
 		ItemStack is = new ItemStack(Material.ENCHANTED_BOOK);
 		ItemMeta im = is.getItemMeta();
 		im.setDisplayName(name);
-		
-		for(String line : lore) {
+
+		for (String line : lore) {
 			line = ChatColor.GRAY + line;
 			realLore.add(line);
 		}
-		
+
 		im.setLore(realLore);
 		is.setItemMeta(im);
 		return is;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -83,8 +86,8 @@ public class CustomEnchantment {
 		return ec;
 	}
 
-	public void setIc(EnchantConfig ec) {
+	public void setEc(EnchantConfig ec) {
 		this.ec = ec;
 	}
-	
+
 }
